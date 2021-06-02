61724b8ed5022e24cad6a85d5e8f7d11e90761f7
immadisairaj <immadirajendra.sai@gmail.com>
2021-06-02 12:04:24 +0530

    update projects

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -9,8 +9,8 @@ class MyConstants extends InheritedWidget {
   final String profileImage = 'assets/Rajendra.jpg';  // location of image (need to be an asset)
   final List<String> headName = const ["Sai Rajendra Immadi"];
   final String tag0 = 'Open-Source Enthusiast | Musician';
-  final String tag1 = 'Mentor and Member @amFOSS';
-  final String about = 'I am Sai Rajendra Immadi, from Hyderabad, B.Tech in CSE at Amrita Vishwa Vidhyapeetham, Amritapuri Campus, India. An open-source enthusiast with proficiency in Android Application Development. Have keen interest to learn new things and able to grasp quickly. I am also a Musician, learning Tabla from age 5.';
+  final String tag1 = 'Intern @IBM';
+  final String about = 'I am Sai Rajendra Immadi, from Hyderabad, B.Tech in CSE at Amrita Vishwa Vidhyapeetham, Amritapuri Campus, India. An open-source enthusiast with proficiency in Mobile Application Development. Have keen interest to learn new things and able to grasp quickly. I am also a Musician, learning Tabla from age 5.';
   final String quote = '"Live the life upto your Maximum"';
 
   // Blog Link
@@ -24,6 +24,7 @@ class MyConstants extends InheritedWidget {
   // if there is anything not needed, then it can be left blank
   // example if link is not needed - ['year', 'projectName', 'subTitle', 'description', ''],
   final List<List<String>> projects = const [
+    ['2021', 'Sai Voice', 'Play Store', 'A mobile radio player which streams audio from Radio Sai Global Harmony', 'https://github.com/immadisairaj/radiosai'],
     ['2020', 'Portfolio Template', '', 'The application allows people to have an online presence with their own personal website with which they can demonstrate their skills and projects', 'https://github.com/immadisairaj/Portfolio'],
     ['2020', 'Distributed Group Chat System', '', 'Terminal based Distributed Group Chat System built in Java using Remote Method Invocation(RMI)', 'https://github.com/immadisairaj/distributedGroupChatSystem'],
     ['2019', 'Harry Potter', '', ' An application built for the fans of Harry Potter to test their luck on the sorting hat and also know the different characters and different spells from Harry Potter', 'https://github.com/immadisairaj/harry_potter'],
@@ -53,8 +54,8 @@ class MyConstants extends InheritedWidget {
     ['2020', 'Distributed Programming in Java', 'by Coursera', 'Credential ID: EV3AM26T2LW8', 'https://www.coursera.org/account/accomplishments/certificate/EV3AM26T2LW8'],
   ];
   final List<List<String>> others = const [
-    ['2019 - Present', 'amFOSS', 'Member and Mentor', 'Active member of the Free and Open Source Software(FOSS) Club at Amrita Vishwa Vidyapeetham. Help mentor juniors in improving their skill set and conduct various sessions and workshops for the same.', ''],
     ['2005 - Present', 'Learning Tabla ', '', 'Completed Certificate Course from Potti Sreeramulu Telugu University and currently pursuing Diploma Course. Gave many individual/group performances.', ''],
+    ['2019 - 2021', 'amFOSS', 'Member and Mentor', 'Active member of the Free and Open Source Software(FOSS) Club at Amrita Vishwa Vidyapeetham. Help mentor juniors in improving their skill set and conduct various sessions and workshops for the same.', ''],
     ['2020', 'Community Kitchen', 'Sri Satya Sai Organization', 'Volunteered to serve in the Community Kitchen arranged by Sri Satya Sai Seva Organizations, Hyderabad in order to serve the needy who were affected by the pandemic. The Community Kitchen served at least 5000 people every day', ''],
     ['2019', 'Student Social Responsibility Project', '', 'Conducted sessions on Education System and future paths into a good career for government school students.', ''],
     ['2018 - 2019', 'Amalabharatam Cleanup Drives', 'Participant', 'Participated in the cleanup drives conducted by the Social Service team of Amrita Vishwa Vidyapeetham.', ''],
